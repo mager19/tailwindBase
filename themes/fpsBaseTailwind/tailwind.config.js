c138c247b80c6c60702b88fe7ce5d12f62816898
Yan <yavallejor@gmail.com>
2020-11-04 10:46:51 -0500

    Version 2.0

--- themes/fpsBaseTailwind/tailwind.config.js
+++ themes/fpsBaseTailwind/tailwind.config.js
@@ -2,12 +2,42 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'test-color': '#C89EC4',
-            }
+                'dark-1'            : '#031b4c',
+                'dark-2'            : '#2b426f',
+                'dark-3'            : '#4f658f',
+                'light-1'           : '#f6f9fd',
+                'light-2'           : '#e7ebf3',
+                'light-3'           : '#92a8d1',
+                'primary'           : '#07358f',
+                'secondary'         : '#e8ac42',
+                'primary--lighter'  : '#3275f5',
+                'primary--medium'   : '#B7CFFF',
+                'secondary--darker' : '#c68f2c',
+                'black'             : '#000',
+                'white'             : '#fff'
+            },
+            fontFamily: { inter: 'Inter' }
         }
     },
-    variants: {},
     plugins: [
-
-    ],
+        function ({ addComponents }) {
+            addComponents({
+                '.container': {
+                    maxWidth: '100%',
+                    '@screen sm': {
+                        maxWidth: '540px',
+                    },
+                    '@screen md': {
+                        maxWidth: '720px',
+                    },
+                    '@screen lg': {
+                        maxWidth: '960px',
+                    },
+                    '@screen xl': {
+                        maxWidth: '1140px',
+                    },
+                }
+            })
+        }
+    ]
 }
