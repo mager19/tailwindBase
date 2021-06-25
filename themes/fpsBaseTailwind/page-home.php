ebc1ae22ba720721f5c90eec567d415c680b5ac2
Yan <yavallejor@gmail.com>
2021-06-25 10:36:37 -0500

    refactor: :recycle: Delete mixins

--- themes/fpsBaseTailwind/page-home.php
+++ themes/fpsBaseTailwind/page-home.php
@@ -18,7 +18,7 @@ get_header(); ?>
 <!-- Hero Header -->
 <?php while (have_posts()) : the_post(); ?>
 
-    <div class="container mx-auto prose-lg py-14">
+    <div class="container mx-auto prose-lg py-14 max-w-[600px]">
         <h3 class="title--1 title-sm--2 title-md--3 title-lg--4 title-xl--5">This is my title</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sit, officiis doloribus nostrum labore eligendi doloremque ducimus enim cupiditate quasi soluta, natus non esse sequi, sapiente adipisci obcaecati. Temporibus, aperiam.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sit, officiis doloribus nostrum labore eligendi doloremque ducimus enim cupiditate quasi soluta, natus non esse sequi, sapiente adipisci obcaecati. Temporibus, aperiam.</p>
